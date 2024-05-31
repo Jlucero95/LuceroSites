10c208e7932ff032a27d1cef62572ada626735e2
Jeremy Lucero <Lucero.jeremy07@gmail.com>
2024-05-30 22:07:38 -0500

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+// import { useState } from "react";
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<div>
+				<a href="">
+					<img
+						src="./LuceroSites.jpg"
+						className="logo"
+						alt="Vite logo"
+					/>
+				</a>
+			</div>
+		</>
+	);
+}
+
+export default App;
